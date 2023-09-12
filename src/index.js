c5fe002037701fa61af934e2ef2c387227d28773
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2023-09-12 23:49:53 +0700

    MVC Controller

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan')
+const {engine}  = require('express-handlebars');
+const app = express();
+const port = 3000
+
+const route = require('./routes')
+
+app.use(express.urlencoded(
+  {
+    extended:true
+  }
+))
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')))
+// HTTP request
+// app.use(morgan('combined'))
+
+// TEMPLATE engine
+app.engine('hbs', engine({extname: '.hbs'}) );
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+console.log(path.join(__dirname, 'resource/views'));
+
+route(app)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
